a48214387798c64f9d86ffca2e68a230581147bf
KateSidorka <125731848+KateSidorka@users.noreply.github.com>
2023-07-22 19:47:37 +0000

    Deploying to gh-pages from @ KateSidorka/goit-js-hw-09@940e6b4cd64254bb76e231a7340cbf4dfbb34b9b 🚀

---
+++ 01-color-switcher.77a21123.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),a=null;function o(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));n.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,a=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.77a21123.js.map
