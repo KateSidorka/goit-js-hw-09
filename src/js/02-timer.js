e61153984eaf6fb8271df59105af343673cb2bd5
KateSidorka <katarinasidorka@gmail.com>
2023-07-23 11:57:10 +0200

    11.57

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,64 +1,13 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
-const datetimePicker = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('[data-start]');
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
 const daysValue = document.querySelector('[data-days]');
 const hoursValue = document.querySelector('[data-hours]');
 const minutesValue = document.querySelector('[data-minutes]');
-const secondsValue = document.querySelector('[data- seconds]');
-
-let countdownIntervalId = null;
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    if (selectedDate < new Date()) {
-      window.alert("Please choose a date in the future");
-      startBtn.disabled = true;
-    } else {
-      startBtn.disabled = false;
-    }
-  },
-};
-
-flatpickr(datetimePicker, options);
-
-startBtn.addEventListener('click', onStartBtnClick);
-
-function onStartBtnClick() {
-  const selectedDate = flatpickr.parseDate(datetimePicker.value);
-  const currentDate = new Date();
-  const timeDifference = selectedDate - currentDate;
-
-  if (timeDifference <= 0) {
-    window.alert("Please choose a date in the future");
-    return;
-  }
-
-  startBtn.disabled = true;
-  countdownIntervalId = setInterval(updateCountdown, 1000, timeDifference);
-}
-
-function updateCountdown(timeDifference) {
-  const { days, hours, minutes, seconds } = convertMs(timeDifference);
-
-  daysValue.textContent = formatTimeValue(days);
-  hoursValue.textContent = formatTimeValue(hours);
-  minutesValue.textContent = formatTimeValue(minutes);
-  secondsValue.textContent = formatTimeValue(seconds);
-
-  timeDifference -= 1000;
-
-  if (timeDifference < 0) {
-    clearInterval(countdownIntervalId);
-    startBtn.disabled = false;
-  }
-}
+const secondsValue = document.querySelector('[data-seconds]');
 
 function convertMs(ms) {
   const second = 1000;
@@ -74,6 +23,51 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-function formatTimeValue(value) {
+function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate < new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(datetimePicker, options);
+
+startButton.addEventListener('click', () => {
+  const selectedDate = flatpickr.parseDate(datetimePicker.value);
+  const currentDate = new Date();
+  const difference = selectedDate - currentDate;
+
+  if (difference <= 0) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+  }
+
+  const timerInterval = setInterval(() => {
+    const remainingTime = convertMs(selectedDate - new Date());
+
+    daysValue.textContent = addLeadingZero(remainingTime.days);
+    hoursValue.textContent = addLeadingZero(remainingTime.hours);
+    minutesValue.textContent = addLeadingZero(remainingTime.minutes);
+    secondsValue.textContent = addLeadingZero(remainingTime.seconds);
+
+    if (remainingTime.days === 0 && remainingTime.hours === 0 && remainingTime.minutes === 0 && remainingTime.seconds === 0) {
+      clearInterval(timerInterval);
+      Notiflix.Notify.success('Countdown finished!');
+    }
+  }, 1000);
+});
